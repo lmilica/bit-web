f8658ddd672977c498f53bc78a79bd938baf1514
BIT Student <classroom@bgit.rs>
2020-02-28 13:02:26 +0100

    new

---
+++ DOM/domInJava.js
@@ -0,0 +1,45 @@
+// var secondUl = function () {
+//     document.getElementById('potato2').className = 'pt2';
+
+
+// }
+// setTimeout(secondUl, 2000);
+// var selectAllLi = function () {
+//     var array = document.getElementsByTagName('li');
+//     for (var i = 0; i < array.length; i++) {
+//         array[i].className = 'pt3';
+//     }
+// }
+// setTimeout(selectAllLi, 1000);
+// var selectLiOfThirdUl = function () {
+//     var array = document.querySelectorAll('#potato3 li');
+//     for (var i = 0; i < array.length; i++) {
+//         array[i].className = 'pt4';
+//     }
+// }
+// setTimeout(selectLiOfThirdUl, 5000);
+
+// var ar = function () {
+//     var items = document.querySelector(".active");
+//     items.className = "";
+//     items.parentElement.parentElement.previousElementSibling.firstElementChild.firstElementChild.className = "active";
+// }
+// setTimeout(ar, 2000);
+
+
+// var nav = function () {
+//     var item = document.querySelector("#sc");
+//     var text = item.textContent;
+//     alert(text)
+// }
+// setTimeout(nav, 3000);
+
+var arr = ["Volvo", "Toyota", "Honda"];
+
+for (var i = 0; i < arr.length; i++) {
+    var optionItem = document.createElement("option");
+    var text = document.createTextNode("cars");
+    listItem.appendChild(text);
+    var someSection = document.querySelector("select");
+    someSection.appendChild()
+}
